051a1c0dec56458d3089eb822cf54d38ec30b211
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2026-01-17 21:52:11 -0300

    fix: :fire: Adicionando Suspense faltante

--- app/transactional-products/page.tsx
+++ app/transactional-products/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import NewTransactionalProductFormModal from "../components/new-transactional-product-form-modal";
 import TransactionalProductsTable from "../components/transactional-products-table";
 
 export default function TransactionalProductsPage() {
   return (
-    <section className="">
+    <section>
       <div className="flex flex-col space-y-4">
         <div className="flex-1 flex flex-row justify-between">
           <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-4">
@@ -11,7 +12,9 @@ export default function TransactionalProductsPage() {
           </h1>
           <NewTransactionalProductFormModal />
         </div>
-        <TransactionalProductsTable />
+        <Suspense>
+          <TransactionalProductsTable />
+        </Suspense>
       </div>
     </section>
   );
